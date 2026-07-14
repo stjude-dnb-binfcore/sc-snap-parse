8ccd7fa6157eee4dfdf77576da9dc95eef4ad9b2
AntoniaChroni <achroni@noderome336.cm.cluster>
2026-07-14 12:20:59 -0500

    Minor updates for cell type annotations

--- analyses/cell-types-annotation/run-cell-types-annotation.sh
+++ analyses/cell-types-annotation/run-cell-types-annotation.sh
@@ -9,7 +9,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 ################################################################################################################
 # Run module
 Rscript --vanilla run-cell-types-annotation-SingleR.R
-#Rscript --vanilla run-cell-types-annotation-gene-markers.R
+Rscript --vanilla run-cell-types-annotation-gene-markers.R
 #Rscript --vanilla run-cell-types-annotation-reference.R
 Rscript --vanilla run-merge-cell-types-annotations-all.R
 ################################################################################################################
